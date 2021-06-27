8c09eaf26788fdc0338a500722e1eb9526aa85d2
zuozhuo <zuoyizhuo@live.com>
2021-06-27 23:55:46 +0800

    message rename

--- src/wallets/SOL/modules/dappProvider/inpage.js
+++ src/wallets/SOL/modules/dappProvider/inpage.js
@@ -1,15 +1,15 @@
 // - https://github.com/project-serum/sol-wallet-adapter#usage
+// - https://docs.phantom.app/
 // check usage: kermit.exchange/static/js/utils/wallet.tsx
 /*
 import SolWalletAdapter from '@project-serum/sol-wallet-adapter';
-// https://github.com/project-serum/sol-wallet-adapter
 
 const wallet = new SolWalletAdapter(window.sollet, network); // sollet injected: window.sollet
 const wallet = window.solana; // phantom injected: window.solana
 
 // solletAdapter only
-wallet.connected;
-wallet.sign();
+wallet.connected = true;
+wallet.sign(); // sign text
 wallet.isInjectedProvider();
 
 // both phantom and solletAdapter include
@@ -20,12 +20,14 @@ wallet.disconnect();
 wallet.connect();
 wallet.signTransaction(transaction);
 wallet.signAllTransactions(transactionsAndSigners);
-wallet.autoApprove();
+wallet.autoApprove = true;
 
 // phantom only
 wallet.isPhantom = true;
 wallet.isConnected = true;
-wallet.request();
+wallet.request(); // low level method
+wallet.openBridge(); // low level method
+wallet.postMessage(); // low level method
 solana.listeners('disconnect')
 
 // check sign, signTransaction, signAllTransactions at sollet PopupPage.js
@@ -36,14 +38,19 @@ import { CONST_DAPP_MESSAGE_TYPES } from '../../../../consts/consts';
 
 function init() {
   window.sollet = {
-    hello: 'world',
+    isOneKey: true,
+    isSollet: true,
 
     postMessage: (message) => {
+      /*
+      sol-wallet-adapter call disconnect() at beforeunload event
+          window.addEventListener('beforeunload', this.disconnect.bind(this));
+       */
       console.log('RPC (Dapp -> Ext)', message);
       const listener = (event) => {
         if (event.detail.id === message.id) {
           window.removeEventListener(
-            CONST_DAPP_MESSAGE_TYPES.CONTENT_TO_INPAGE,
+            CONST_DAPP_MESSAGE_TYPES.EVENT_CONTENT_TO_INPAGE,
             listener,
           );
           // send message to dapp (sol-wallet-adapter)
@@ -52,14 +59,14 @@ function init() {
         }
       };
       window.addEventListener(
-        CONST_DAPP_MESSAGE_TYPES.CONTENT_TO_INPAGE,
+        CONST_DAPP_MESSAGE_TYPES.EVENT_CONTENT_TO_INPAGE,
         listener,
       );
 
       window.dispatchEvent(
         new window.CustomEvent(
           // sollet_injected_script_message
-          CONST_DAPP_MESSAGE_TYPES.INPAGE_TO_CONTENT,
+          CONST_DAPP_MESSAGE_TYPES.EVENT_INPAGE_TO_CONTENT,
           {
             detail: message,
           },
