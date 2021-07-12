c688ecf953dab0a8cca8b219ef480dfca704c737
zuozhuo <zuoyizhuo@live.com>
2021-07-12 18:05:42 +0800

    add solana app dropdown item

--- src/utils/syncReduxToMobxMiddleware.js
+++ src/utils/syncReduxToMobxMiddleware.js
@@ -1,12 +1,19 @@
+import storeApp from '../store/storeApp';
+
 const syncReduxToMobxMiddleware = (store) => (next) => (action) => {
   // console.log('dispatching', action);
 
   // eslint-disable-next-line node/callback-return
   const result = next(action);
 
-  // const newState = store.getState();
-  // console.log('next state', newState);
-  // console.log('isUnlocked', newState.metamask.isUnlocked);
+  const newState = store.getState();
+  if (newState.metamask) {
+    const { isUnlocked, selectedAddress } = newState.metamask;
+    storeApp.legacyState = {
+      isUnlocked,
+      selectedAddress,
+    };
+  }
 
   return result;
 };
