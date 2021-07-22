29cb376f072ba16f3cd1d318fcafff987d8ec299
zuozhuo <zuoyizhuo@live.com>
2021-07-22 21:48:25 +0800

    scssify test

--- development/build/configs.js
+++ development/build/configs.js
@@ -1,5 +1,5 @@
 const sassConfig = {
-  includePaths: ['src'],
+  includePaths: ['src', 'ui'],
 
   // Enable both of these to get source maps working
   // "browserify --debug" will also enable css sourcemaps
@@ -10,6 +10,41 @@ const sassConfig = {
   // outputStyle: 'compressed',
 };
 
+const scssifyConfig = {
+  autoInject: {
+    // Useful for debugging; adds data-href="src/foo.scss" to <style> tags
+    verbose: true,
+    // If true the <style> tag will be prepended to the <head>
+    prepend: false,
+  },
+
+  // require('./MyComponent.scss') === '.MyComponent{color:red;background:blue}'
+  // autoInject: false, will also enable this
+  // pre 1.x.x, this is enabled by default
+  export: false,
+
+  // Pass options to the compiler, check the node-sass project for more details
+  sass: {
+    sourceMapEmbed: false,
+    sourceMapContents: false,
+    // This is the default only when opt.sass is undefined
+    outputStyle: 'compressed',
+    ...sassConfig,
+  },
+
+  // Configure postcss plugins too!
+  // postcss is a "soft" dependency so you may need to install it yourself
+  postcss: {
+    autoprefixer: {},
+  },
+
+  postProcess(css) {
+    // allows for processing the generated CSS
+    return css;
+  },
+};
+
 module.exports = {
   sassConfig,
+  scssifyConfig,
 };
