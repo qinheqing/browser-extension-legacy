6e09fdc64690d6714e5fdcf9cefc71d39af50a49
zuozhuo <zuoyizhuo@live.com>
2021-07-27 08:06:06 +0800

    add global error handler for background

---
+++ app/scripts/errorsGlobalHandler.js
@@ -0,0 +1,31 @@
+import { isString } from 'lodash';
+
+function showExtensionNotification(error) {
+  const msg = isString(error) ? error : error.message;
+
+  const showNotification = global?.$$extensionPlatform?._showNotification;
+  return showNotification && msg && showNotification('Uncaught Error', msg);
+}
+
+function init() {
+  window.addEventListener('unhandledrejection', function (event) {
+    // console.error(
+    //   'Unhandled rejection (promise: ',
+    //   event.promise,
+    //   ', reason: ',
+    //   event.reason,
+    //   ').',
+    // );
+    showExtensionNotification(event.reason);
+  });
+
+  console.log('window.addEventListener#error');
+  window.addEventListener('error', (event) => {
+    console.log('window.addEventListener on error');
+    showExtensionNotification(event.error);
+  });
+}
+
+export default {
+  init,
+};
