02e9519576c9f49bf2e2efe8efe53d560ae9ce5b
kwoktung <onlyif2009@gmail.com>
2021-08-06 11:44:02 +0800

    switch token on token transfer flow

---
+++ src/components/TokenSwitchDialog/index.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { observer } from 'mobx-react-lite';
+import { Dialog, Transition } from '@headlessui/react';
+import { cloneDeep } from 'lodash';
+import AppIcons from '../AppIcons';
+import OneDialog from '../OneDialog';
+import storeTransfer from '../../store/storeTransfer';
+import storeToken from '../../store/storeToken';
+import OneCellItem from '../OneCellItem';
+import { TokenLogoIcon } from '../LogoIcon';
+
+function TokenOption({ token, onClick, checked = false }) {
+  const { symbol } = token;
+  const { name, icon } = token;
+  const tokenName = token.symbolOrName || token.contractAddressShort;
+
+  const end = checked ? (
+    <div className="text-right">
+      <AppIcons.CheckIcon className="w-4 h-4" />
+    </div>
+  ) : (
+    <span className="w-4 h-4" />
+  );
+
+  return (
+    <OneCellItem
+      className="px-4"
+      onClick={() => onClick(token)}
+      start={<TokenLogoIcon tokenInfo={token} className="" />}
+      end={end}
+      title={<div className="text-lg relative">{tokenName}</div>}
+    />
+  );
+}
+
+function TokenSwitchDialog({ open, onOpenChange }) {
+  const tokens = storeToken.currentTokens;
+  const { fromToken } = storeTransfer;
+
+  const [selectedAddress, setSelectedAddress] = useState(fromToken.address);
+
+  const onConfirm = () => {
+    const selected = tokens.filter(
+      (token) => token.address === selectedAddress,
+    )[0];
+    if (selected) {
+      storeTransfer.fromToken = cloneDeep(selected);
+    }
+  };
+
+  return (
+    <OneDialog
+      overlayClose={false}
+      open={open}
+      title="选择资产"
+      confirmText="确认"
+      onOpenChange={onOpenChange}
+      onConfirm={onConfirm}
+    >
+      <div className="py-3 -mx-4 ">
+        {tokens.map((token, index) => {
+          return (
+            <TokenOption
+              key={token.contractAddress + index}
+              token={token}
+              onClick={(e) => setSelectedAddress(e.address)}
+              checked={selectedAddress === token.address}
+            />
+          );
+        })}
+      </div>
+    </OneDialog>
+  );
+}
+
+TokenSwitchDialog.propTypes = {
+  children: PropTypes.any,
+};
+
+export default observer(TokenSwitchDialog);
