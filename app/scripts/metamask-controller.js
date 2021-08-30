fa808dc8f9a410ae27fb821c89e376fb313567d6
zuozhuo <zuoyizhuo@live.com>
2021-08-30 15:32:01 +0800

    add ledger keyring

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -14,6 +14,7 @@ import { Mutex } from 'await-semaphore';
 import ethUtil from 'ethereumjs-util';
 import log from 'loglevel';
 import OneKeyKeyring from '@onekeyhq/eth-onekey-keyring';
+import LedgerBridgeKeyring from '@metamask/eth-ledger-bridge-keyring';
 import EthQuery from 'eth-query';
 import nanoid from 'nanoid';
 import {
@@ -215,7 +216,11 @@ export default class MetamaskController extends EventEmitter {
       preferencesController: this.preferencesController,
     });
 
-    const additionalKeyrings = [OneKeyKeyring, AddressKeyring];
+    const additionalKeyrings = [
+      OneKeyKeyring,
+      AddressKeyring,
+      LedgerBridgeKeyring,
+    ];
     this.keyringController = new KeyringController({
       keyringTypes: additionalKeyrings,
       initState: initState.KeyringController,
