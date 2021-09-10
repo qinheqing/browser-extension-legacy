eac0b72993f6e5bf08093e668f45dc94692c9625
zuozhuo <zuoyizhuo@live.com>
2021-09-10 12:04:54 +0800

    add Google Analytics

---
+++ development/build/googleAnalyticsCode.js
@@ -0,0 +1,19 @@
+const head = `
+<!-- Google Tag Manager -->
+<script type="text/javascript"  src="/vendor/gtm.js"></script>
+<!-- End Google Tag Manager -->
+`;
+
+const body = `
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WG6T9PN"
+ height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
+`;
+
+module.exports = {
+  head,
+  body,
+};
