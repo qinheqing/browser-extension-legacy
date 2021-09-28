ae4a162a4eb86c023b6921f39817bcca1e461c5d
zhao.zhang <loatheb.zhao@gmail.com>
2021-09-28 11:14:18 +0800

    feat: add ui-components support

--- ui/app/pages/unlock-page/unlock-page.component.js
+++ ui/app/pages/unlock-page/unlock-page.component.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
+import Button from '@onekeyhq/ui-components/Button';
+
 import TextField from '../../components/ui/text-field';
 import { DEFAULT_ROUTE } from '../../helpers/constants/routes';
 
@@ -98,8 +100,8 @@ export default class UnlockPage extends Component {
 
     return (
       <Button
-        type="submit"
-        style={style}
+        type="primary"
+        // style={style}
         disabled={!this.state.password}
         fullWidth
         variant="contained"
