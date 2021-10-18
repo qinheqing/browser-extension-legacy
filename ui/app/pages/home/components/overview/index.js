cc540bd673755565eee6db99f9e147a340f2c0d6
kwoktung <onlyif2009@gmail.com>
2021-10-18 16:31:48 +0800

    redesign setting pages

--- ui/app/pages/home/components/overview/index.js
+++ ui/app/pages/home/components/overview/index.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react';
+import { useHistory } from 'react-router-dom';
 import MenuBar from '../../../../components/app/menu-bar';
 import AppHeader from '../../../../components/app/app-header';
 import { EthOverview } from '../../../../components/app/wallet-overview';
 import AssetList from '../../../../components/app/asset-list';
 import { ASSET_ROUTE } from '../../../../helpers/constants/routes';
 
-export default class Overview extends Component {
-  render() {
-    return (
-      <div className="home__container">
-        <AppHeader />
-        <div className="home__main-view">
-          <MenuBar />
-          <div className="home__balance-wrapper">
-            <EthOverview />
-          </div>
-          <div className="home__asset-wrapper">
-            <div className="home__asset-title">Assets</div>
-            <div className="home__asset-content">
-              <AssetList
-                onClickAsset={(asset) =>
-                  history.push(`${ASSET_ROUTE}/${asset}`)
-                }
-              />
-            </div>
+const Overview = () => {
+  const history = useHistory();
+  return (
+    <div className="home__container">
+      <AppHeader />
+      <div className="home__main-view">
+        <MenuBar />
+        <div className="home__balance-wrapper">
+          <EthOverview />
+        </div>
+        <div className="home__asset-wrapper">
+          <div className="home__asset-title">Assets</div>
+          <div className="home__asset-content">
+            <AssetList
+              onClickAsset={(asset) => history.push(`${ASSET_ROUTE}/${asset}`)}
+            />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Overview;
