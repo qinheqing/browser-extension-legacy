72a63b02104cfe1c1ac851837c687d209aa9ab6d
kwoktung <onlyif2009@gmail.com>
2021-10-21 14:19:57 +0800

    redesign send page

--- ui/app/components/app/sidebars/sidebar.component.js
+++ ui/app/components/app/sidebars/sidebar.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransition } from 'react-transition-group';
+import Popover from '../../ui/popover-beta';
 import CustomizeGas from '../gas-customization/gas-modal-page-container';
 
 export default class Sidebar extends Component {
@@ -14,35 +15,6 @@ export default class Sidebar extends Component {
     onOverlayClose: PropTypes.func,
   };
 
-  renderOverlay() {
-    const { onOverlayClose } = this.props;
-
-    return (
-      <div
-        className="sidebar-overlay"
-        onClick={() => {
-          onOverlayClose?.();
-          this.props.hideSidebar();
-        }}
-      />
-    );
-  }
-
-  renderSidebarContent() {
-    const { type, sidebarProps = {} } = this.props;
-    const { transaction = {} } = sidebarProps;
-    switch (type) {
-      case 'customize-gas':
-        return (
-          <div className="sidebar-left">
-            <CustomizeGas transaction={transaction} />
-          </div>
-        );
-      default:
-        return null;
-    }
-  }
-
   componentDidUpdate(prevProps) {
     if (!prevProps.sidebarShouldClose && this.props.sidebarShouldClose) {
       this.props.hideSidebar();
@@ -50,22 +22,15 @@ export default class Sidebar extends Component {
   }
 
   render() {
-    const { transitionName, sidebarOpen, sidebarShouldClose } = this.props;
+    const { sidebarOpen, hideSidebar, sidebarProps = {} } = this.props;
+    const { transaction = {} } = sidebarProps;
 
     return (
-      <div>
-        <CSSTransition
-          classNames={transitionName}
-          timeout={{ exit: 200, enter: 300 }}
-        >
-          <>
-            {sidebarOpen && !sidebarShouldClose
-              ? this.renderSidebarContent()
-              : null}
-          </>
-        </CSSTransition>
-        {sidebarOpen && !sidebarShouldClose ? this.renderOverlay() : null}
-      </div>
+      sidebarOpen && (
+        <Popover onClose={hideSidebar}>
+          <CustomizeGas transaction={transaction} />
+        </Popover>
+      )
     );
   }
 }
